f661845f5d57e4d59013d9aa3472b22613eaf255
mydadisalive <mydadisalive@gmail.com>
2020-01-09 16:14:19 +0200

    Fixed forgotten ! in shabengs

--- set_web_hook.sh
+++ set_web_hook.sh
@@ -1,3 +1,3 @@
-#/usr/bin/bash
+#!/usr/bin/bash
 
 curl --request POST --url https://api.telegram.org/bot${TELEGRAM_TOKEN}/setWebhook --header 'content-type: application/json' --data '{"url": "https://ma83ilgzuc.execute-api.us-east-1.amazonaws.com/dev/bot"}'
