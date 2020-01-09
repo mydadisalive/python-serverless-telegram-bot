b0db223c4923cee67ee02ee9b672e93c8807fdf4
EC2 Default User <ec2-user@ip-172-31-83-142.ec2.internal>
2020-01-09 13:54:23 +0000

    Initial commit

---
+++ logs.sh
@@ -0,0 +1,1 @@
+sls logs -f post -t
---
+++ set_web_hook.sh
@@ -0,0 +1,1 @@
+curl --request POST --url https://api.telegram.org/bot${TELEGRAM_TOKEN}/setWebhook --header 'content-type: application/json' --data '{"url": "https://ma83ilgzuc.execute-api.us-east-1.amazonaws.com/dev/bot"}'
